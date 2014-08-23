bbb03a4039789ed18dbf25ea17a46f6499f4176b
philipbrown <phil@ipbrown.com>
2014-08-23 16:26:20 +0100

    Clean up UserTest

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -31,7 +31,7 @@ class UserTest extends PHPUnit_Framework_TestCase {
     $collection = $this->model->all();
 
     $this->assertInstanceOf('Illuminate\Support\Collection', $collection);
-    //$this->assertTrue(count($collection) == 2);
+    $this->assertEquals(2, $collection->count());
 
     $this->assertInstanceOf('PhilipBrown\CapsuleCRM\User', $collection[0]);
     $this->assertEquals('2', $collection[0]->id);
@@ -42,15 +42,13 @@ class UserTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals('true', $collection[0]->logged_in);
     $this->assertEquals('100', $collection[0]->party_id);
 
-    /*
-    $this->assertInstanceOf('PhilipBrown\CapsuleCRM\User', $collection[0]);
-    $this->assertEquals('3', $collection[0]->id);
-    $this->assertEquals('j.joe', $collection[0]->username);
-    $this->assertEquals('Jane Doe', $collection[0]->name);
-    $this->assertEquals('GBP', $collection[0]->currency);
-    $this->assertEquals('Europe/London', $collection[0]->timezone);
-    $this->assertEquals('101', $collection[0]->party_id);
-    */
+    $this->assertInstanceOf('PhilipBrown\CapsuleCRM\User', $collection[1]);
+    $this->assertEquals('3', $collection[1]->id);
+    $this->assertEquals('j.joe', $collection[1]->username);
+    $this->assertEquals('Jane Doe', $collection[1]->name);
+    $this->assertEquals('GBP', $collection[1]->currency);
+    $this->assertEquals('Europe/London', $collection[1]->timezone);
+    $this->assertEquals('101', $collection[1]->party_id);
   }
 
 }
