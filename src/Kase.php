387ff8cac5d5766bcfaa1e0d0bafa1d2089c78b8
philipbrown <phil@ipbrown.com>
2014-08-23 14:37:33 +0100

    Add the Kase model and tests

---
+++ src/Kase.php
@@ -0,0 +1,50 @@
+<?php namespace PhilipBrown\CapsuleCRM;
+
+use PhilipBrown\CapsuleCRM\Querying\Findable;
+
+class Kase extends Model {
+
+  use Findable;
+  use Serializable;
+
+  /**
+   * The model's fillable attributes
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'id',
+    'status',
+    'name',
+    'description',
+    'party_id',
+    'owner',
+    'created_on',
+    'updated_on'
+  ];
+
+  /**
+   * The model's queryable options
+   *
+   * @var array
+   */
+  protected $queryableOptions = [
+    'singular' => 'kase',
+    'plural' => 'kase'
+  ];
+
+  /**
+   * Create a new instance of the model
+   *
+   * @param PhilipBrown\CapsuleCRM\Connection $connection
+   * @param array $attributes;
+   * @return void
+   */
+  public function __construct(Connection $connection,  array $attributes = [])
+  {
+    parent::__construct($connection);
+
+    $this->fill($attributes);
+  }
+
+}
---
+++ tests/KaseTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use Mockery as m;
+use PhilipBrown\CapsuleCRM\Kase;
+
+class KaseTest extends PHPUnit_Framework_TestCase {
+
+  /** @var PhilipBrown\CapsuleCRM\Connection */
+  private $connection;
+
+  /** @var PhilipBrown\CapsuleCRM\Kase */
+  private $case;
+
+  /** @var Guzzle\Http\Message\Response */
+  private $message;
+
+  public function setUp()
+  {
+    $this->connection = m::mock('PhilipBrown\CapsuleCRM\Connection');
+    $this->case = new Kase($this->connection);
+    $this->message = m::mock('Guzzle\Http\Message\Response');
+  }
+
+  /** @test */
+  public function should_require_connection()
+  {
+    $this->setExpectedException('Exception');
+
+    $c = new Kase('');
+  }
+
+  /** @test */
+  public function find_case_by_id()
+  {
+    $response = file_get_contents(dirname(__FILE__).'/stubs/kase.json');
+    $this->message->shouldReceive('json')->andReturn(json_decode($response, true));
+    $this->connection->shouldReceive('get')->andReturn($this->message);
+
+    $case = $this->case->find(43);
+
+    $this->assertInstanceOf('PhilipBrown\CapsuleCRM\Kase', $case);
+    $this->assertEquals('43', $case->id);
+    $this->assertEquals('OPEN', $case->status);
+    $this->assertEquals('Consulting', $case->name);
+    $this->assertEquals('Scope and design web site shopping cart', $case->description);
+    $this->assertEquals('2', $case->party_id);
+    $this->assertEquals('a.user', $case->owner);
+    $this->assertEquals('2011-04-16T13:59:58Z', $case->created_on);
+    $this->assertEquals('2011-05-11T16:54:23Z', $case->updated_on);
+  }
+
+  /** @test */
+  public function find_all_cases()
+  {
+    $response = file_get_contents(dirname(__FILE__).'/stubs/kases.json');
+    $this->message->shouldReceive('json')->andReturn(json_decode($response, true));
+    $this->connection->shouldReceive('get')->andReturn($this->message);
+
+    $collection = $this->case->all();
+
+    $this->assertInstanceOf('Illuminate\Support\Collection', $collection);
+    $this->assertTrue(count($collection) == 1);
+    $this->assertInstanceOf('PhilipBrown\CapsuleCRM\Kase', $collection[0]);
+  }
+
+}
